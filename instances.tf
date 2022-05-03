684b9fba8fb55eac9d6c9adda40c4481f062343f
crazymanya <abond22@hotmail.com>
2022-05-02 22:01:15 -0300

    Prueba Bondanza / Arrospide

--- instances.tf
+++ instances.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "ac1-instance" {
-  ami                    = "ami-03ededff12e34e59e"
+  ami                    = "ami-0022f774911c1d690"
   instance_type          = "t2.micro"
+  subnet_id = aws_subnet.ac1-private-subnet.id
+
   vpc_security_group_ids = [aws_security_group.ac1-sg.id]
-  key_name               = "vockey"
+  key_name               = "laboratorio"
   tags = {
     Name      = "ac1-instance"
     terraform = "True"
@@ -11,7 +13,7 @@ resource "aws_instance" "ac1-instance" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("~/Documents/ORT/labsuser.cer")
+    private_key  = file("/home/crazymanya/Prueba/ac1-alvaro-s-bondanza-arrospide/laboratorio.pem")
     host        = self.public_ip
   }
   provisioner "remote-exec" {
--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "perfil" {
+  default = "AlvaroB"
 }
 
 variable "region" {
